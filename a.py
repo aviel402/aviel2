6490d5c37b4c04ab1bca45227b407464c01061f1
אביאל <x0583289789@gmail.com>
2025-08-27 17:49:12 +0300

    h

--- a.py
+++ a.py
@@ -1,16 +1,9 @@
-
 from flask import Flask, request
-
+import text
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def calculator_main():
     all = request.form if request.form else request.args
-    response_str = b(all)
+    response_str = text.b(all)
     return Response(response_str, mimetype='text/plain; charset=UTF-8')
-
-
-def b(all):
-    text=all.get(txt)
-    text = t- + text
-    reteun text
---
+++ text.py
@@ -0,0 +1,5 @@
+
+def b(all):
+    text=all.get(txt)
+    text = t- + text
+    reteun text
